f4bc17ec444673518670389ed2887d2210d66f6f
Chandan Roy <chaitanyaroy1971@gmail.com>
2026-09-06 12:22:44 +0530

    chore: remove temporary order-processor demo artifacts

--- tax_service.py
+++
@@ -1,7 +0,0 @@
-def get_tax_rate_by_tier(customer_tier: str) -> float:
-    rates = {
-        "STANDARD": 0.10,
-        "PREMIUM": 0.07,
-        "VIP": 0.05
-    }
-    return rates.get(customer_tier.upper(), 0.10)
